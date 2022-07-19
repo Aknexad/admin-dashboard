f5f6ed31d466ddb3891553f4b2b397b7824523c0
Aknexad <aknejad@outlook.com>
2022-07-19 12:39:20 +0430

    add js and style for  announcements and trending  button

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ ann.addEventListener('click', (element) => {
     'flex';
   document.querySelector('.trending').style.display =
     'none';
+  ann.style.backgroundColor = '#fff';
+  ann.style.color = '#06040d';
+  ann.style.boxShadow = '2px 4px 8px black inset';
+
+  tre.style.backgroundColor = '#06040d';
+  tre.style.color = '#fff';
 });
 tre.addEventListener('click', (element) => {
   document.querySelector('.trending').style.display =
     'flex';
   document.querySelector('.announcements').style.display =
     'none';
+  tre.style.backgroundColor = '#fff';
+  tre.style.color = '#06040d';
+  tre.style.boxShadow = '2px 4px 8px black inset';
+
+  ann.style.backgroundColor = '#06040d';
+  ann.style.color = '#fff';
 });
